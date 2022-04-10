a0eea0ac3e11edc488aa9fbd2348f31cd7d51f6b
kpost34 <kpost342005@hotmail.com>
2022-04-10 16:13:11 -0600

    Initial commit of empty app.R script and newly generated 'backbone' R scripts

---
+++ anova_assumptions_code.R
@@ -0,0 +1,34 @@
+# R code to develop Shiny app for running ANOVAs or K-W tests
+# Part 1 of 3: exploratory tables and plots, ANOVA assumptions, and transformations
+
+
+#load packages
+library(ggplot2)
+
+#### Exploratory data analysis------------------------------------------------------------------------
+
+
+
+#### ANOVA assumptions--------------------------------------------------------------------------------
+##test assumptions graphically and statistically
+#graph
+
+
+
+#create functions for assumption testing
+
+
+
+#### Data transformations & ANOVA assumptions---------------------------------------------------------
+
+
+
+
+#more functions?
+
+
+
+
+
+
+
---
+++ anova_testing_code.R
@@ -0,0 +1,2 @@
+#R code to develop Shiny app for running ANOVAs or K-W tests
+# Part 2 of 3: ANOVA testing and post-hoc tests
