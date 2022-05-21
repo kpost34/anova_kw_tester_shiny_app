858238d043ce25e30e125616f09675c1817ed57f
kpost34 <kpost342005@hotmail.com>
2022-05-21 15:27:16 -0600

    Updated sourcing code

--- anova_kw_tester_app/app.R
+++ anova_kw_tester_app/app.R
@@ -1,6 +1,5 @@
 #load packages
 library(shiny)
-library(here)
 library(dplyr)
 library(tibble)
 library(ggplot2)
@@ -10,7 +9,7 @@ library(rstatix)
 library(bslib)
 
 #load functions
-source(here("anova_kw_tester_app","anova_kw_tester_app_functions.R"))
+source("anova_kw_tester_app_functions.R")
     
 #create list for summary tables
 stat_list<-list(n=length,min=min,median=median,mean=mean,max=max,sd=sd,se=function(x) sd(x)/sqrt(length(x)))
